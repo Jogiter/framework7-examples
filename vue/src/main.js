bd4e9b0b8312883581b394457bfff9f5b91543b4
jogiter <jogiter.g@gmail.com>
2022-03-31 11:25:06 +0800

    feat(vue): replace alias and update f7 imports

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
-import Framework7 from 'framework7/lite/bundle'
+import Framework7 from 'framework7/lite-bundle'
 import Framework7Vue from 'framework7-vue'
 import App from './App.vue'
 
-import 'framework7/css/bundle'
+import 'framework7/framework7-bundle.min.css'
 import './css/app.css'
 
 // Demo
--- vue/vite.config.js
+++ vue/vite.config.js
@@ -5,10 +5,4 @@ import { visualizer } from 'rollup-plugin-visualizer'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), visualizer()],
-  resolve: {
-    alias: {
-      'framework7/css/bundle': 'framework7/framework7-bundle.css',
-      'framework7/lite/bundle': 'framework7/lite-bundle',
-    },
-  },
 })
